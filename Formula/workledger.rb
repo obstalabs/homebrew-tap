413f53e3778aef0e2261b509ce8c14458ed03661
obstalabs <265053889+obstalabs@users.noreply.github.com>
2026-05-06 13:16:09 +0000

    Brew formula update for workledger version v0.15.0

--- Formula/workledger.rb
+++ Formula/workledger.rb
@@ -5,29 +5,29 @@
 class Workledger < Formula
   desc "Hiveram ledger plane for work orders, claims, release bundles, and WEO attribution"
   homepage "https://hiveram.com"
-  version "0.14.2"
+  version "0.15.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.14.2/workledger_0.14.2_darwin_amd64.tar.gz"
-      sha256 "37dbcb28715e31016e9d47e91a4f827591d64f02669c3e80d95222f2a6738d6a"
+      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.15.0/workledger_0.15.0_darwin_amd64.tar.gz"
+      sha256 "f4fe37356a849bbe78320a9d037f9a727331e0505385f922cb08257e97986252"
     end
 
     on_arm do
-      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.14.2/workledger_0.14.2_darwin_arm64.tar.gz"
-      sha256 "fb9c26d6177ec17a8416a5ac46eaf7c83df308dd6434a060855ff5eb9edf2f35"
+      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.15.0/workledger_0.15.0_darwin_arm64.tar.gz"
+      sha256 "926f31d69ff47948483ce7e30de87552deb4c55aa2421f4471f5e6aaef4efd3c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.14.2/workledger_0.14.2_linux_amd64.tar.gz"
-      sha256 "c911c57ffe52f1c5a27f054628aae22aba03637996f45b7edef73bd116c89e1a"
+      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.15.0/workledger_0.15.0_linux_amd64.tar.gz"
+      sha256 "28e2643637de18ff154a45dd5756815eddc2558a3080435cb51d29a49219a7d5"
     end
 
     on_arm do
-      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.14.2/workledger_0.14.2_linux_arm64.tar.gz"
-      sha256 "93bef4491e2045a0e838da0aa8ecc928016d0bb03c3faf0ded4983b9f6c51902"
+      url "https://github.com/obstalabs/hiveram-dist/releases/download/v0.15.0/workledger_0.15.0_linux_arm64.tar.gz"
+      sha256 "b7b93cd584f8e232ffdc11192e0e5f3f62055d2195d62b7b57acbab5769607d9"
     end
   end
 
